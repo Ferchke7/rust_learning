2a8e2b941577ce5c9c94db2c6f13e7b439747ada
ferdavs <ferdavs@naver.com>
2024-10-07 22:01:39 +0900

    trait

--- main.rs
+++ main.rs
@@ -1,13 +1,16 @@
 fn main() {
     let tom = Person { name: String::from("Tom"), age: 36 };
-    tom.print();
+    tom.print_multiple_copies(3);
 }
 struct Person { name: String, age: u8 }
 trait Printer{
-    fn print(&self);
+    fn print_multiple_copies(&self, times: u8);
 }
 impl Printer for Person {
-    fn print(&self) {
-        println!("Person {}; age {}", self.name, self.age);
+    fn print_multiple_copies(&self, mut number: u8){
+        while number > 0 {
+            println!("Person {}; age: {}",self.name, self.age);
+            number-=1;
+        }
     }
 }
