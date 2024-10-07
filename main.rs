64d4eea11e11af040de57fff9652fc1197213034
ferdavs <ferdavs@naver.com>
2024-10-07 21:03:23 +0900

    OOP 2

--- main.rs
+++ main.rs
@@ -1,29 +1,23 @@
 fn main() {
-    let tom = Person{
+    let mut tom = Person{
         name: "Tom".to_string(),
         age: 36
     };
-    tom.display();
-    tom.display();
-    tom.say_hello(&tom, 19);
     let bob = Person { name: "Bob".to_string(), age: 41 };
-    bob.display();
+    let is_older = tom.is_older(&bob);
+    if is_older{
+        println!("STARSHE");
+    }
+    else {
+        println!("ISMAIL PETUH");
+    }
 }
 struct Person {
     name: String,
     age: u8
 }
 impl Person {
-    fn say_hello(&self, other: &Person, hour: u8) {
-        if hour < 16{
-            println!("Hi, {}!", other.name);
-        }
-        else {
-            println!("Good evening, {}", other.name);
-        }
-    }
-
-    fn display(&self){
-        println!("Name: {} Age {}", &self.name, &self.age)
+    fn is_older(&mut self, other: &Person) -> bool {
+        self.age > other.age
     }
 }
