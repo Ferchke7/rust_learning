4f26b07c25c04ae989188ad28acdacf72684f09a
ferdavs <ferdavs@naver.com>
2024-10-09 16:10:00 +0900

    enum with generics

--- main.rs
+++ main.rs
@@ -1,10 +1,15 @@
-struct Person<T> {
-    id: T,
-    name: String
+enum DayTime<T>{
+      
+    Morning(T),
+    Evening(T)
 }
-
-fn main() {
-    let tom = Person { id: 245, name: "Tom".to_string() };
-    println!("id: {} name {}", tom.id, tom.name);
-
+ 
+fn main(){
+     
+    let morning = DayTime::Morning("Доброе утро".to_string());  // параметр T представляет тип String
+    if let DayTime::Morning(morning_value)= morning { println!("Morning: {}", morning_value);}
+     
+      
+    let evening = DayTime::Evening(16);     // параметр T представляет тип i32
+    if let DayTime::Evening(evening_value)= evening { println!("Evening: {}", evening_value);}
 }
