225f0e84a318042db7b5c8ffcd003a48c45f22ca
ferdavs <ferdavs@naver.com>
2024-10-09 19:07:41 +0900

    using dyn to designate the type

--- main.rs
+++ main.rs
@@ -1,45 +1,29 @@
-struct Circle { radius: f64 }
-struct Rectangle { width: u32, height: u32 }
+trait Shape{
+    fn area(&self) -> f64;
+}
+struct Circle{
+    radius: f64
+}
+struct Rectangle{
+    width: f64,
+    height: f64
+}
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        self.radius * self.radius * 3.14
+    }
+}
+impl Shape for Rectangle{
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+}
 
-trait Shape {
-    type Unit;
-    fn area(&self)->Self::Unit;
-}
-impl Shape for Circle{
-    type Unit = f64;
-    fn area(&self) -> Self::Unit { self.radius * self.radius * 3.14 }
-}
-impl Shape for Rectangle {
-    type Unit = u32;
-    fn area(&self) -> Self::Unit { self.width * self.height }
-}
 fn main() {
-    let circle = Circle{radius: 10.0};
-    println!("Area = {} ", circle.area());
-    let rect = Rectangle{width:10, height:20};
-    println!("Area = {}", rect.area());
+    let mut my_shape: &dyn Shape = &Circle { radius: 5.0 }; //decide what trait object save a link to the address
+    println!("Circle, {}", my_shape.area());
 
-}
-//Alternative way to assocative type
-struct Circle;
-struct Rectangle;
-trait Shape {}
-impl Shape for Circle{ }
-impl Shape for Rectangle{ }
- 
-struct CircleGeometry;
-struct RectangleGeometry;
- 
-trait Geometry{
-    type ShapeType: Shape;
-    fn create_shape()-> Self::ShapeType;
-}
- 
-impl Geometry for CircleGeometry{
-    type ShapeType = Circle;
-    fn create_shape() -> Self::ShapeType { Circle}
-}
-impl Geometry for RectangleGeometry{
-    type ShapeType = Rectangle;
-    fn create_shape() -> Self::ShapeType { Rectangle}
+    my_shape = &Rectangle { width: 10.0, height: 20.0 };
+    println!("Rectangle {}", my_shape.area());
+
 }
