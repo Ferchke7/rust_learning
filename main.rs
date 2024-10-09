557bbef65dd5e0d1bd43f51399dcce0469b9876f
ferdavs <ferdavs@naver.com>
2024-10-09 20:47:46 +0900

    HashMaps

--- main.rs
+++ main.rs
@@ -1,37 +1,9 @@
+use std::collections::HashMap;
 
-enum Phones {
-    Number(u64),
-    Text(String)
-}
 fn main() {
-    let v: Vec<i32> = Vec::new();
-    let c = vec![1,2,3];
-    let j: Vec<i32> = vec![];
-    //default fill the values
-    let vs = vec![5;3];
-    let mut users = Vec::new();
-    users.push("Tom");
-    users.push("Sam");
-    users.push("Bob");
-    users[0] = "FER";
-
-    let secondindex = users.get(1);
-    match secondindex {
-        Some(user) => println!("is user {}", user),
-        None => println!("User not found")
-    }   
-    for user in &users{
-        println!("{}", user);
-    }
-    let phones = vec![
-        Phones::Number(123131213),
-        Phones::Text(String::from("soetete"))
-    ];
-
-    for phone in &phones {
-        match phone {
-            Phones::Number(n) => println!("{n}"),
-            Phones::Text(x) => println!("{x}")
-        }
-    }
+    let people = HashMap::from([
+        ("Alice",35),
+        ("Tom", 39)
+    ]);
+    println!("{:?}", people);
 }
