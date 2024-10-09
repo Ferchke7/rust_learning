2a7869314ce28faae510d02b3a6420786321680f
ferdavs <ferdavs@naver.com>
2024-10-09 16:18:16 +0900

    Generics function

--- main.rs
+++ main.rs
@@ -1,17 +1,10 @@
-struct Circle {
-    radius: f64,
-}
-trait Drawable<T> {
-    fn draw(&self, figure:&T);
-}
-struct CircleGui;
-impl Drawable<Circle> for CircleGui{
-    fn draw(&self, figure: &Circle){
-        println!("Draw a circle {}", figure.radius);
-    }
-}
 fn main() {
-    let gui = CircleGui{};
-    let circle = Circle { radius: 2.0 };
-    gui.draw(&circle);
+    let results1 = receive(3);
+    println!("{}", results1);
+
+    let results2 = receive("somet");
+    println!("{}", results2);
+}
+fn receive<T>(item: T) -> T{
+    item
 }
