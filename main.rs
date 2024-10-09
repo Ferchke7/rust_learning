afc2735c1a3ad5369ec4e27de2f620f110d7aa50
ferdavs <ferdavs@naver.com>
2024-10-09 18:36:40 +0900

    loading traits

--- main.rs
+++ main.rs
@@ -1,34 +1,20 @@
-truct Message { text: String}
-struct Person { name: String}
- 
-trait Printer{  fn print(&self); }
-  
-impl Printer for Message{
-    fn print(&self){
-        println!("Message text: {}", self.text);
+use std::ops::Add;
+
+struct Counter{
+    value: u32
+}
+impl Add for Counter{
+    type Output = Counter; // Generating type of value
+    fn add(self, other: Counter) -> Counter{
+        Counter {
+            value: self.value + other.value
+        }
     }
 }
- 
-impl Printer for Person{
-    fn print(&self){
-        println!("Person name: {}", self.name);
-    }
-}
- 
-trait ConsolePrinter{  fn console_print(&self); }
-impl<T:Printer> ConsolePrinter for T{
-    fn console_print(&self){
-        println!("******Печать на консоль*****");
-        self.print();
-        println!(); // для отделения строк при выводе на консоль
-    }
-}
- 
-fn main(){
-      
-    let mes = Message {text: String::from("Hello METANIT.COM")};
-    mes.console_print();
- 
-    let tom = Person{name: String::from("Tom")};
-    tom.console_print();
+
+fn main() {
+    let counter1 = Counter{value: 5};
+    let counter2 = Counter{value: 15};
+    let counter3 = counter1 + counter2;
+    println!("{}", counter3.value);
 }
