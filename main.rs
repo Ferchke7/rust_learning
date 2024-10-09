e05714130423202770a4afc8d1da3581f099ee02
ferdavs <ferdavs@naver.com>
2024-10-09 16:25:49 +0900

    compare

--- main.rs
+++ main.rs
@@ -1,22 +1,20 @@
 struct Person<T>{
+ 
     id: T,
     name: String
-}
-impl<T> Person<T>{
-    fn get_id(&self) -> &T{
-        &self.id
-    }
 }
 impl Person<u32>{
     fn compare_id(&self, user_id: u32) -> bool{
         self.id == user_id
     }
 }
-fn main() {
-    let user = Person {id: "be something", name: String::from("1") };
-    
-    let anotherUser = Person {id: 1, name: "Tom".to_string() };
-    println!("{}",user.get_id());
-    println!("{}",anotherUser.get_id());
-    
+fn main(){
+     
+    let tom = Person{id:1, name: String::from("Tom")};
+    let result1 = tom.compare_id(1);
+    println!("result1: {}", result1);   // result1: true
+     
+    let bob = Person{id:4, name: String::from("Bob")};
+    let result2 = bob.compare_id(1);
+    println!("result2: {}", result2);   // result2: false
 }
